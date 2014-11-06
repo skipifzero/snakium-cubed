e7968f1b84c3fc4531a76a2265c5317701626c63
Peter Hillerström <peter@hstroem.se>
2014-11-06 22:14:24 +0100

    Added initial linking of SDL2 and simple test code.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
 
 int main(int argc, char** argv)
 {
-	std::cout << "snakium³ comming soon" << std::endl;
+	SDL_Init(SDL_INIT_VIDEO);
+	IMG_Init(IMG_INIT_PNG);
+
+	SDL_Window* window = SDL_CreateWindow("snakium³",
+	                                      SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+	                                      400, 400, SDL_WINDOW_SHOWN);
+
+	SDL_Delay(2000);
+
+	SDL_DestroyWindow(window);
+	IMG_Quit();
+	SDL_Quit();
 	return 0;
 }
