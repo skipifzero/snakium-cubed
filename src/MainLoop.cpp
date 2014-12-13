d762cfbfeb957df6ced37a9851b4a283fa643085
Peter Hillerström <peter@hstroem.se>
2014-12-14 00:11:44 +0100

    MainLoop: Added simple delta time calculations using std::chrono::high_resolution_clock.

--- src/MainLoop.cpp
+++ src/MainLoop.cpp
@@ -2,5 +2,44 @@
 
 namespace s3 {
 
+namespace {
+
+float calculateDelta() noexcept
+{
+	static std::chrono::high_resolution_clock::time_point previousTime, currentTime;
+
+	previousTime = currentTime;
+	currentTime = std::chrono::high_resolution_clock::now();
+
+	using FloatSecondDuration = std::chrono::duration<float>;
+	return std::chrono::duration_cast<FloatSecondDuration>(currentTime - previousTime).count();
+}
+
+} // anonymous namespace
+
+MainLoop::MainLoop(std::function<bool(float)> update, std::function<bool(float)> render) noexcept
+:
+	updateFunc(update),
+	renderFunc(render)
+{
+	// Initialization done.
+}
+
+MainLoop::~MainLoop() noexcept
+{
+
+}
+
+void MainLoop::run()
+{
+	float delta = calculateDelta(); // Call calculateDelta() here to initialize counting.
+	
+	while (true) {
+		delta = calculateDelta();
+
+		if(updateFunc(delta)) return;
+		if(renderFunc(delta)) return;
+	}
+}
 
 } // namespace s3
--- src/MainLoop.hpp
+++ src/MainLoop.hpp
@@ -2,8 +2,27 @@
 #ifndef S3_MAIN_LOOP_HPP
 #define S3_MAIN_LOOP_HPP
 
+#include <sfz/SDL.hpp>
+#include <sfz/GL.hpp>
+#include <functional> // std::function
+#include <chrono> // std::chrono::high_resolution_clock
+
 namespace s3 {
 
+class MainLoop final {
+public:
+	MainLoop() = delete;
+	MainLoop(const MainLoop&) = delete;
+	MainLoop& operator= (const MainLoop&) = delete;
+
+	MainLoop(std::function<bool(float)> update, std::function<bool(float)> render) noexcept;
+	~MainLoop() noexcept;
+	void run();
+
+private:
+	std::function<bool(float)> updateFunc;
+	std::function<bool(float)> renderFunc;
+};
 
 } // namespace s3
 
