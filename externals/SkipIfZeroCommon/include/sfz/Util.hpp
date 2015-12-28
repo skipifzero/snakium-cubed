325f0c70222f933cf10c5436e7add5bb6c03f408
Peter Hillerström <peter@hstroem.se>
2015-12-28 01:02:54 +0100

    Moved FrametimeStats to SFZCommon.

--- externals/SkipIfZeroCommon/include/sfz/Util.hpp
+++ externals/SkipIfZeroCommon/include/sfz/Util.hpp
@@ -2,6 +2,7 @@
 #ifndef SFZ_UTIL_HPP
 #define SFZ_UTIL_HPP
 
+#include "sfz/util/FrametimeStats.hpp"
 #include "sfz/util/IniParser.hpp"
 #include "sfz/util/IO.hpp"
 #include "sfz/util/StopWatch.hpp"
