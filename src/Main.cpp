25e6d689c03d340ec24be167a27e7711bb4d31f3
Peter Hillerström <peter@hstroem.se>
2016-01-01 17:22:07 +0100

    Workaround frustrating errors with SDL_GL_SwapWindow(). For some unexplicable reason it calls deprecated OpenGL functions, which causes the debug output to fire a ton of errors.

--- src/Main.cpp
+++ src/Main.cpp
@@ -50,10 +50,16 @@ int main()
 	
 	// Creates OpenGL context, debug if SFZ_NO_DEBUG is not defined
 	SDL_GL_SetAttribute(SDL_GL_FRAMEBUFFER_SRGB_CAPABLE, 1);
+	const int MAJOR_VERSION = 4;
+	const int MINOR_VERSION = 1;
 #if !defined(SFZ_NO_DEBUG)
-	gl::Context glContext{window.mPtr, 4, 1, gl::GLContextProfile::CORE, true};
+#ifdef _WIN32
+	gl::Context glContext{window.mPtr, MAJOR_VERSION, MINOR_VERSION, gl::GLContextProfile::COMPATIBILITY, true};
 #else
-	gl::Context glContext{window.mPtr, 4, 1, gl::GLContextProfile::CORE, false};
+	gl::Context glContext{window.mPtr, MAJOR_VERSION, MINOR_VERSION, gl::GLContextProfile::CORE, true};
+#endif
+#else
+	gl::Context glContext{window.mPtr, MAJOR_VERSION, MINOR_VERSION, gl::GLContextProfile::CORE, false};
 #endif
 	
 	gl::printSystemGLInfo();
