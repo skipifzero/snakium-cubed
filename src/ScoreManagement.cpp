daa2a64d626acb0548d674d08d48b68b16fb1141
Peter Hillerström <peter@hstroem.se>
2016-02-17 21:57:13 +0100

    More untested score management functions.

---
+++ src/ScoreManagement.cpp
@@ -0,0 +1,125 @@
+#include "ScoreManagement.hpp"
+
+#include <cstdint>
+#include <cstring>
+#include <iostream>
+#include <string>
+
+#include <sfz/util/IO.hpp>
+
+namespace s3 {
+
+using std::string;
+using std::uint8_t;
+
+// statics
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+static const char* scoresPath() noexcept
+{
+	static const string PATH = sfz::gameBaseFolderPath() + "/snakium-cubed/highscores.bin";
+	return PATH.c_str();
+}
+
+// General functions
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+ScoreBundle createEmptyScoreBundle() noexcept
+{
+	ScoreBundle tmp;
+	for (size_t i = 0; i < NUM_SCORES_SAVED; ++i) {
+		for (size_t n = 0; n < SCORE_NAME_LENGTH + 1; ++n) {
+			tmp.standardNames[i][n] = '\0';
+			tmp.largeNames[i][n] = '\0';
+			tmp.giantNames[i][n] = '\0';
+		}
+	}
+	return tmp;
+}
+
+int tryAddScoreToBundle(ScoreBundle& currentHighScores, ScoreConfigType configType,
+                        Stats newScore, const char* scoreName) noexcept
+{
+	int32_t score = 0;
+	Stats* scores = nullptr;
+	char (*names)[SCORE_NAME_LENGTH + 1] = nullptr;
+	size_t* numScores = nullptr;
+	const ModelConfig* mc = nullptr;
+	switch (configType) {
+	case ScoreConfigType::STANDARD:
+		score = totalScore(newScore, STANDARD_CONFIG);
+		scores = currentHighScores.standardResults;
+		names = currentHighScores.standardNames;
+		numScores = &currentHighScores.numStandardResults;
+		mc = &STANDARD_CONFIG;
+		break;
+	case ScoreConfigType::LARGE:
+		score = totalScore(newScore, LARGE_CONFIG);
+		scores = currentHighScores.largeResults;
+		names = currentHighScores.largeNames;
+		numScores = &currentHighScores.numLargeResults;
+		mc = &LARGE_CONFIG;
+		break;
+	case ScoreConfigType::GIANT:
+		score = totalScore(newScore, GIANT_CONFIG);
+		scores = currentHighScores.giantResults;
+		names = currentHighScores.giantNames;
+		numScores = &currentHighScores.numGiantResults;
+		mc = &GIANT_CONFIG;
+		break;
+	}
+
+	// Just add score if list is not filled
+	if (*numScores < NUM_SCORES_SAVED) {
+		scores[*numScores] = newScore;
+		std::memcpy(names[*numScores], scoreName, SCORE_NAME_LENGTH);
+		*numScores += 1;
+		return (int)*numScores;
+	}
+
+	// Insert score if better
+	for (size_t i = 0; i < NUM_SCORES_SAVED; ++i) {
+		int32_t ithScore = totalScore(scores[i], *mc);
+		if (ithScore < score) {
+			for (size_t j = i; j < NUM_SCORES_SAVED-1; ++j) {
+				scores[j+1] = scores[j];
+				std::memcpy(names[j+1], names[j], SCORE_NAME_LENGTH);
+			}
+			scores[i] = newScore;
+			std::memcpy(names[i], scoreName, SCORE_NAME_LENGTH);
+			return (int)i+1;
+		}
+	}
+
+	return -1;
+}
+
+// IO functions
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+bool writeScores(const ScoreBundle& scores) noexcept
+{
+	return sfz::writeBinaryFile(scoresPath(), (const uint8_t*)&scores, sizeof(ScoreBundle));
+}
+
+bool loadScores(ScoreBundle& scoresOut) noexcept
+{
+	ScoreBundle tmp;
+	int32_t res = sfz::readBinaryFile(scoresPath(), (uint8_t*)&tmp, sizeof(ScoreBundle));
+	if (res == -2) {
+		std::cerr << "Corrupt score file." << std::endl;
+		return false;
+	}
+	if (res == -1) {
+		return false;
+	}
+	scoresOut = tmp;
+	return true;
+}
+
+bool deleteScores() noexcept
+{ 
+	return sfz::deleteFile(scoresPath());
+}
+
+} // namespace s3
---
+++ src/ScoreManagement.hpp
@@ -0,0 +1,68 @@
+#pragma once
+#ifndef S3_SCORE_MANAGEMENT_HPP
+#define S3_SCORE_MANAGEMENT_HPP
+
+#include <cstddef>
+
+#include "gamelogic/Stats.hpp"
+
+namespace s3 {
+
+using std::size_t;
+
+// Constants
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+constexpr size_t NUM_SCORES_SAVED = 5;
+constexpr size_t SCORE_NAME_LENGTH = 15;
+
+enum class ScoreConfigType {
+	STANDARD,
+	LARGE,
+	GIANT
+};
+
+// Bundle struct
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+struct ScoreBundle final {
+	Stats standardResults[NUM_SCORES_SAVED];
+	char standardNames[NUM_SCORES_SAVED][SCORE_NAME_LENGTH + 1];
+	size_t numStandardResults = 0;
+	
+	Stats largeResults[NUM_SCORES_SAVED];
+	char largeNames[NUM_SCORES_SAVED][SCORE_NAME_LENGTH + 1];
+	size_t numLargeResults = 0;
+
+	Stats giantResults[NUM_SCORES_SAVED];
+	char giantNames[NUM_SCORES_SAVED][SCORE_NAME_LENGTH + 1];
+	size_t numGiantResults = 0;
+};
+
+// General functions
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+ScoreBundle createEmptyScoreBundle() noexcept;
+
+/**
+ * @brief Attempts to add new high score to the current ones
+ * @param currentHighScores the current scores bundle
+ * @param configType the config the new score belongs to
+ * @param newScore the new score
+ * @param scoreName the name of the new score (must be at least SCORE_NAME_LENGTH large buffer)
+ * @return the ranking of the new score, -1 if not ranked
+ */
+int tryAddScoreToBundle(ScoreBundle& currentHighScores, ScoreConfigType configType,
+                        Stats newScore, const char* scoreName) noexcept;
+
+// IO functions
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+
+bool writeScores(const ScoreBundle& scores) noexcept;
+
+bool loadScores(ScoreBundle& scoresOut) noexcept;
+
+bool deleteScores() noexcept;
+
+} // namespace s3
+#endif
